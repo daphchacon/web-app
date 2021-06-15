1aaee5caa6811502163f90885b1d39dc4aeac6f4
daphchacon <85708601+daphchacon@users.noreply.github.com>
2021-06-15 16:26:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.get("/", async (req, res) => {
 
 // 👇 add requiresAuth middlware to these private routes  👇
 
-app.get("/user",requiresAuth(), async (req, res) => {
+app.get("/user", requiresAuth(), async (req, res) => {
   res.render("user", {
     user: req.oidc && req.oidc.user,
     id_token: req.oidc && req.oidc.idToken,
@@ -77,7 +77,7 @@ app.get("/user",requiresAuth(), async (req, res) => {
   });
 });
 
-app.get("/expenses",requiresAuth(), async (req, res, next) => {
+app.get("/expenses", requiresAuth(), async (req, res, next) => {
   res.render("expenses", {
     user: req.oidc && req.oidc.user,
     expenses,
